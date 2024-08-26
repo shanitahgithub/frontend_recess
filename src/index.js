0093f1246db182c5d582144e0cc5771a8ab4f277
shanitahgithub <nakaggshanitah874@gmail.com>
2024-08-26 11:58:59 +0300

    Recess Frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Set base URL for Axios
+axios.defaults.baseURL = "http://localhost:5000/users"; // Replace with your backend URL if different
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
